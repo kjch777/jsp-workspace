7a077e8f408545e8ac5d331261966b1c7e78616f
kjch777 <kjch911229@naver.com>
2024-09-09 10:16:55 +0900

    JSP

---
+++ chap1_JSP/src/com/kh/JSP_개요.java
@@ -0,0 +1,12 @@
+package com.kh;
+/**
+ * JSP: Java Server Pages
+ * Html 코드 안에서 Java 코드를 작성할 수 있다.
+ * html 페이지 안에 모든 Java 코드를 작성하는 것이 아니라, 부분적으로 필요한 기능만 Java 로 작성하는 것이다.
+ * JSP 와 같은 프로젝트를 진행하기 위해서는, ThymeLeaf 와 같은 환경으로 사용해야 한다.
+ * Dynamic Web Project(옛날 방식) 또는 Springboot 환경으로 실행한다.
+ * ThymeLeaf -> JSP 형식으로 작성해주면 된다.
+ * **/
+public class JSP_개요 {
+
+}
